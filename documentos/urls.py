d66482564dce490c78a05579b852e166c5575bd7
Lucas Alexsander <lucas10barbosa_@hotmail.com>
2024-05-06 10:58:43 -0300

    CRUD documentos

--- documentos/urls.py
+++ documentos/urls.py
@@ -4,6 +4,11 @@ from . import views
 urlpatterns = [
     path('pdfs/<str:filename>/', views.view_pdf, name='view_pdf'),
     path('', views.listar_documentos, name='listar_documentos'),
+    path('adicionar/', views.adicionar_documento, name='adicionar_documento'),
+    path('atualizar/<int:documento_id>/', views.atualizar_documento, name='atualizar_documento'),
+    path('deletar/<int:documento_id>/', views.deletar_documento, name='deletar_documento'),
+    
+    # API
     path('api/documentos/', views.listar_documentosAPI, name='listar_documentosAPI'),
     path('api/<int:pk>/', views.detalhe_documento, name='detalhe_documento'),
 ] 
--- documentos/views.py
+++ documentos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Documentos
+from . forms import DocumentosForm
 from django.http import FileResponse, HttpResponseNotFound, JsonResponse
 from django.urls import reverse
 from documentos.functions import handle_uploaded_file
@@ -24,6 +25,57 @@ def view_pdf(request, filename):
 def listar_documentos(request):
     documentos = Documentos.objects.all()
     return render(request, 'documentosDataTable.html', {'documentos': documentos})
+
+def adicionar_documento(request):
+    form = DocumentosForm(request.POST, request.FILES)
+    if request.method == 'POST':
+        form = DocumentosForm(request.POST, request.FILES)
+        if form.is_valid(): 
+            handle_uploaded_file(request.FILES['arquivo'])
+            model_instace = form.save(commit=False)
+            model_instace.save()
+            model_instace.autor.set(form.cleaned_data['autor'])
+            model_instace.orientador.set(form.cleaned_data['orientador'])
+            model_instace.coorientador.set(form.cleaned_data['coorientador'])
+            
+            documentos = Documentos.objects.all()
+            return redirect('/documentos', {'Documentos': documentos})
+    else:
+        form = DocumentosForm()
+        return render(request, 'adicionar_documento.html', {'form': form})
+        
+    return render(request, 'adicionar_documento.html', {'form': form})
+
+def atualizar_documento(request, documento_id=None):  # Aceita o parâmetro documento_id
+    # Se o documento_id for fornecido, recuperar o documento correspondente
+    documento = None
+    if documento_id is not None:
+        documento = Documentos.objects.get(pk=documento_id)
+
+    if request.method == 'POST':
+        form = DocumentosForm(request.POST, request.FILES, instance=documento)  # Passa a instância do documento para o formulário
+        if form.is_valid(): 
+            form.save()
+            
+            # Criar um json onde vai salvar o que foi atualizado
+            # Ex: id = 1, data = DATENOW(), alteração= 'Nome da monografia alterado'
+            # Além de renderizar no front a data da atualização
+            # Criar uma rota para mostrar todas as alterações que foram feitas em determinado item
+            
+            documentos = Documentos.objects.all()
+            return redirect('/documentos', {'Documentos': documentos})
+    else:
+        form = DocumentosForm(instance=documento)  # Passa a instância do documento para o formulário
+
+    documentos = Documentos.objects.all()
+    return render(request, 'adicionar_documento.html', {'form': form, 'documentos': documentos})
+
+def deletar_documento(request, documento_id):
+    documento = get_object_or_404(Documentos, id=documento_id)
+    documento.delete()
+
+    documentos = Documentos.objects.all()
+    return redirect('/documentos', {'Documentos': documentos})
 
 @swagger_auto_schema(method='get', responses={200: openapi.Response("List of Documentos", DocumentosSerializer(many=True))}, tags=['documentos'])
 @api_view(['GET'])
