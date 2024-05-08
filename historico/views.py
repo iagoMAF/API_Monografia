fe063087bfaf2a73e9dc0902ce85707802741460
Lucas Alexsander <lucas10barbosa_@hotmail.com>
2024-05-08 08:27:14 -0300

    Otimiza o código de histórico de alterações

--- historico/views.py
+++ historico/views.py
@@ -38,75 +38,69 @@ def remover_historico(usuario, tabela, id_elemento):
     
 def atualiza_historico(usuario, tabela, itemAtualizado, itemAntigo):
     atualizacao = "Atualização: " 
-    
-    if tabela == 'Usuário':
-        
-        if itemAntigo.username != itemAtualizado.username:
-            atualizacao = atualizacao + 'username, '
-        if itemAntigo.first_name != itemAtualizado.first_name:
-            atualizacao = atualizacao + 'nome, '
-        if itemAntigo.last_name != itemAtualizado.last_name:
-            atualizacao = atualizacao +'sobrenome, '
-        if itemAntigo.email != itemAtualizado.email:
-            atualizacao = atualizacao + 'e-mail, '
-        if itemAntigo.is_staff != itemAtualizado.is_staff:
-            atualizacao = atualizacao + 'nível de usuário, '
-        if itemAntigo.password != itemAtualizado.password:
-            atualizacao = atualizacao +'senha, '
-            
-        if atualizacao.endswith(", "):
-            atualizacao = atualizacao[:-2]
 
+    if tabela == 'Equipe':
+        campos = [
+            {'campo': 'nome', 'nome_exibicao': 'nome'},
+            {'campo': 'nivel', 'nome_exibicao': 'nível'},
+            {'campo': 'lattes', 'nome_exibicao': 'lattes'},
+            {'campo': 'linkedin', 'nome_exibicao': 'linkedin'},
+            {'campo': 'researchgate', 'nome_exibicao': 'research gate'},
+            {'campo': 'email', 'nome_exibicao': 'e-mail'},
+            {'campo': 'ativo', 'nome_exibicao': 'status de ativo'},
+        ]
+    elif tabela == 'Documento':
+        campos = [           
+            {'campo': 'titulo', 'nome_exibicao': 'título'},
+            {'campo': 'autor', 'nome_exibicao': 'autor'},
+            {'campo': 'orientador', 'nome_exibicao': 'orientador'},
+            {'campo': 'coorientador', 'nome_exibicao': 'coorientador'},
+            {'campo': 'resumo', 'nome_exibicao': 'resumo'},
+            {'campo': 'dataEntrega', 'nome_exibicao': 'data de entrega'},
+            {'campo': 'arquivo', 'nome_exibicao': 'arquivo'},
+            {'campo': 'notaFinal', 'nome_exibicao': 'nota final'},
+            {'campo': 'areaConcentracao', 'nome_exibicao': 'área de concentração'},
+            {'campo': 'palavrasChaves', 'nome_exibicao': 'palavras-chave'}
+        ] 
+    elif tabela == 'Usuário':
+        campos = [
+            {'campo': 'username', 'nome_exibicao': 'username'},
+            {'campo': 'first_name', 'nome_exibicao': 'nome'},
+            {'campo': 'last_name', 'nome_exibicao': 'sobrenome'},
+            {'campo': 'email', 'nome_exibicao': 'e-mail'},
+            {'campo': 'is_staff', 'nome_exibicao': 'nível de usuário'},
+            {'campo': 'password', 'nome_exibicao': 'senha, '},
+        ]
+    
+    for campo_info in campos:
+        campo = campo_info['campo']
+        nome_exibicao = campo_info['nome_exibicao']
+        
+        if tabela == 'Usuário':
+            valor_atualizado = getattr(itemAtualizado, campo)
+        else:
+            valor_atualizado = getattr(itemAtualizado.instance, campo)
+        valor_antigo = getattr(itemAntigo, campo)
+
+        # Compara os valores dos campos atualizado e antigo
+        print('Novo: ' + str(valor_atualizado))
+        print('Antigo: ' + str(valor_antigo))       
+        
+        if valor_atualizado != valor_antigo:
+            atualizacao += f"{nome_exibicao}, "
+
+    if atualizacao.endswith(", "):
+        atualizacao = atualizacao[:-2]
+        
+    if tabela == 'Usuário':
         Historico.objects.create(
             usuario=usuario,
             tabela=tabela,
             idElemento=itemAtualizado.id,
             tipoAcao='U',
             descAcao=atualizacao
-        )
+        )  
     else:
-        if tabela == 'Equipe':
-            campos = [
-                {'campo': 'nome', 'nome_exibicao': 'nome'},
-                {'campo': 'nivel', 'nome_exibicao': 'nível'},
-                {'campo': 'lattes', 'nome_exibicao': 'lattes'},
-                {'campo': 'linkedin', 'nome_exibicao': 'linkedin'},
-                {'campo': 'researchgate', 'nome_exibicao': 'research gate'},
-                {'campo': 'email', 'nome_exibicao': 'e-mail'},
-                {'campo': 'ativo', 'nome_exibicao': 'status de ativo'},
-            ]
-        elif tabela == 'Documento':
-            campos = [           
-                {'campo': 'titulo', 'nome_exibicao': 'título'},
-                {'campo': 'autor', 'nome_exibicao': 'autor'},
-                {'campo': 'orientador', 'nome_exibicao': 'orientador'},
-                {'campo': 'coorientador', 'nome_exibicao': 'coorientador'},
-                {'campo': 'resumo', 'nome_exibicao': 'resumo'},
-                {'campo': 'dataEntrega', 'nome_exibicao': 'data de entrega'},
-                {'campo': 'arquivo', 'nome_exibicao': 'arquivo'},
-                {'campo': 'notaFinal', 'nome_exibicao': 'nota final'},
-                {'campo': 'areaConcentracao', 'nome_exibicao': 'área de concentração'},
-                {'campo': 'palavrasChaves', 'nome_exibicao': 'palavras-chave'}
-            ]  
-        
-        for campo_info in campos:
-            campo = campo_info['campo']
-            nome_exibicao = campo_info['nome_exibicao']
-            
-            valor_atualizado = getattr(itemAtualizado.instance, campo)
-            valor_antigo = getattr(itemAntigo, campo)
-
-            # Compara os valores dos campos atualizado e antigo
-            
-            print('Novo: ' + str(valor_atualizado))
-            print('Antigo: ' + str(valor_antigo))
-            
-            if valor_atualizado != valor_antigo:
-                atualizacao += f"{nome_exibicao}, "
-
-        if atualizacao.endswith(", "):
-            atualizacao = atualizacao[:-2]
-
         Historico.objects.create(
             usuario=usuario,
             tabela=tabela,
