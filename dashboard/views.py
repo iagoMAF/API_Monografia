e79468ed89fc3a64f79a070f67244717b63cbcc0
Mariano Silva <76002553+Mariano-SI@users.noreply.github.com>
2024-06-08 14:57:54 -0300

    feat: 4 primeiros gráficos OK

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from documentos.models import Documentos
 from documentos.forms import DocumentosForm
 from equipe.models import Pesquisador
+from django.db.models import Count
 
 
 
@@ -18,11 +19,29 @@ def show_dashboard(request):
     titulacoes = [choice[0] for choice in Pesquisador._meta.get_field('nivel').choices]
     pesquisadores_por_titulacao = [Pesquisador.objects.filter(nivel=titulacao).count() for titulacao in titulacoes]
 
+    #Para o grafico de docuemntos por nota
+
+    documentos_por_nota = Documentos.objects.exclude(notaFinal__isnull=True).values('notaFinal').annotate(total=Count('id')).order_by('-notaFinal')
+    notas = [item['notaFinal'] for item in documentos_por_nota]
+    quantidade_documentos = [item['total'] for item in documentos_por_nota]
+
+    #Quantidade de doumentos por autor
+    autores = Pesquisador.objects.annotate(num_documentos=Count('autor_requests_created')).order_by('-num_documentos')
+    nomes_autores = [autor.nome for autor in autores]
+    documentos_por_autor = [autor.num_documentos for autor in autores]
+
+
     context = {
         'areas': areas,
         'documentos_por_area': documentos_por_area,
         'titulacoes': titulacoes,
         'pesquisadores_por_titulacao': pesquisadores_por_titulacao,
+        'notas': notas,
+        'quantidade_documentos': quantidade_documentos,
+        'nomes_autores': nomes_autores,
+        'documentos_por_autor': documentos_por_autor
     }
+    
+
 
     return render(request, 'dashboard.html', context)
