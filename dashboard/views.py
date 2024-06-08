760c6a9727d257f234496ca59be637950f471a2c
Mariano Silva <76002553+Mariano-SI@users.noreply.github.com>
2024-06-08 14:19:49 -0300

    feat: primeiros graficos implementados

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect, get_object_or_404
 from documentos.models import Documentos
 from documentos.forms import DocumentosForm
+from equipe.models import Pesquisador
 
 
 
@@ -9,4 +10,19 @@ from documentos.forms import DocumentosForm
 
 # Create your views here.
 def show_dashboard(request):
-    return render(request, 'dashboard.html');
+    # Para o gráfico de barras
+    areas = [choice[0] for choice in Documentos._meta.get_field('areaConcentracao').choices]
+    documentos_por_area = [Documentos.objects.filter(areaConcentracao=area).count() for area in areas]
+
+    # Para o gráfico de pizza
+    titulacoes = [choice[0] for choice in Pesquisador._meta.get_field('nivel').choices]
+    pesquisadores_por_titulacao = [Pesquisador.objects.filter(nivel=titulacao).count() for titulacao in titulacoes]
+
+    context = {
+        'areas': areas,
+        'documentos_por_area': documentos_por_area,
+        'titulacoes': titulacoes,
+        'pesquisadores_por_titulacao': pesquisadores_por_titulacao,
+    }
+
+    return render(request, 'dashboard.html', context)
